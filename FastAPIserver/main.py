d8fa0ec4849979ffb48432763c4bfcc25cf16d1e
A-Cronnex <aaron18c1@gmail.com>
2025-03-05 13:35:58 -0500

    added sockets

--- FastAPIserver/main.py
+++ FastAPIserver/main.py
@@ -2,20 +2,20 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-stored_data = {}
+stored_data = "sample"
 
 @app.get("/")
 async def root():
     return {"message: Hello World"}
 
-@app.get("/api/command")
+@app.get("/command/")
 async def getCommand():
-    return {"command":stored_data["command"]}
+    return {"command":stored_data}
 
-@app.post("/api")
+@app.post("/api/")
 async def postToRecord(command):
     global stored_data
-    stored_data["command"] = command.command
+    stored_data = command.command
     return {"status": "Got it!"}
 
 
--- main.py
+++ main.py
@@ -3,10 +3,13 @@ from picozero import RGBLED
 from utilities import convert, uid
 from constants import *
 from micropython import const
+import requests
+import json
+import network
 #Inicializar pines del LED
 
 import uos
-
+            
 # Get filesystem statistics for the root directory
 stats = uos.statvfs("/")
 
@@ -30,10 +33,18 @@ import bluetooth
                      # Pairing seguro)
 
 
-
+#Red WLAN
 nic = network.WLAN(network.WLAN.IF_STA)
 nic.active(True)
 current_networks = nic.scan()
+wlan_networks = {
+    
+    "house_network" : {"ssid":"","key":""},
+    "college_network": {"ssid":"","key":""}
+    
+    
+    }
+
 
 rgb = RGBLED(red = RED_PIN, green = GREEN_PIN, blue = BLUE_PIN)
 
@@ -45,7 +56,7 @@ device_info = aioble.Service(GENERIC)
 connection = None
 
 #Creación de características
-aioble.Characteristic(device_info, bluetooth.UUID(MANUFACTURER_ID), read = True, initial="Aaron's_Rasperry")
+aioble.Characteristic(device_info, bluetooth.UUID(MANUFACTURER_ID), read = True, initial="Aaron's_Raspberry")
 aioble.Characteristic(device_info, bluetooth.UUID(MODEL_NUMBER_ID), read = True, initial="1.0")
 aioble.Characteristic(device_info, bluetooth.UUID(SERIAL_NUMBER_ID), read = True, initial=uid())
 aioble.Characteristic(device_info, bluetooth.UUID(HARDWARE_REVISION_ID), read = True, initial = sys.version)
@@ -75,27 +86,39 @@ async def remote_task():
     while True:
         if not connected:
             print("No se ha conectado")
+            rgb.color = convert(0,0,0)
+            print(nic.isconnected())
             await asyncio.sleep_ms(100)
             continue
         if pButton1.value() == 0 and pButton2.value() == 0:
-            print("Enviando notificación al celular para identificar su posición")
-            rgb.color = (255,255,0)
-            button_characteristic.write(b"a")
-            button_characteristic.notify(connection, b"a")
+            print("Encontrar dispositivo")
+            rgb.color = convert(255,255,0)
+            code = "a".encode("utf-8")
+            button_characteristic.write(code)
+            button_characteristic.notify(connection,code)
+            print("Value a sent")
         elif pButton1.value() == 0:
-            rgb.color = (0,255,255)
+            rgb.color = convert(0,255,255)
             print("Haciendo grabación")
-            button_characteristic.write(b"b")
-            button_characteristic.notify(connection, b"b")
+            code = "b".encode("utf-8")
+            button_characteristic.write(code)
+            button_characteristic.notify(connection,code)
+            print("Notification sent")
         elif pButton2.value() == 0:
-            print("Llamando a emergencias")
-            rgb.color = (255,0,255)
-            button_characteristic.write(b"c")
-            button_characteristic.notify(connection, b"c")
+            print("Guardando grabacion")
+            rgb.color = convert(255,0,255)
+            code = "c".encode("utf-8")
+            button_characteristic.write(code)
+            button_characteristic.notify(connection, code)
+            print("Value sent")
         else:
             print("No se ha enviado nada de información")
-            button_characteristic.write(b"d")
-            button_characteristic.notify(connection, b"d")
+            rgb.color = convert(255,255,255)
+            code = "d".encode("utf-8")
+            button_characteristic.write(code)
+            button_characteristic.notify(connection,code)
+            print(f"Value {code}")
+            
         await asyncio.sleep_ms(500)
 
 async def periphereal_task():
@@ -104,7 +127,7 @@ async def periphereal_task():
         connected = False
         async with await aioble.advertise(
             ADV_INTERNAL_MS,
-            name = "Aaron's_Rasperry",
+            name = "Aaron's_Raspberry",
             appearance = BLE_APPEREANCE_GENERIC_REMOTE_CONTROL,
             services = [GENERIC]
         ) as connection:
@@ -114,18 +137,103 @@ async def periphereal_task():
             await connection.disconnected()
             print("disconnected")
             
+            
+async def connect_to_wifi():
+    
+    if not nic.isconnected():
+        nic.connect(wlan_networks["house_network"]["ssid"],wlan_networks["house_network"]["key"])
+        
+
+def create_request(command,url,api_endpoint,method):
+    
+    headers = """\
+    {method} /{api_endpoint} HTTP/1.1\r\n
+    Content-Type: {content_type}\r\n
+    Content-Length: {content_length}\r\n
+    Host: {host}\r\n
+    Connection: close\r\n
+    \r\n"""
+
+    port = 8000
+
+    body = f'{{"command": "{command}"}}'
+    print(body)
+
+    body_bytes = body.encode('utf-8')
+    header_bytes = headers.format(
+        method=method,
+        api_endpoint=api_endpoint,
+        content_type="application/json",
+        content_length=len(body_bytes),
+        host=f"{url}:{port}"
+    )
+
+    print(header_bytes)
+    print(body_bytes)
+
+    payload = header_bytes.encode('utf-8') + body_bytes
+
+    return payload
+        
+async def send_to_API():
+    
+    import socket
+    
+    url = "192.168.50.199"
+    addr = socket.getaddrinfo(url,8000)[0][-1]
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)            
+    s.connect(addr)
+    print("Connected!")
+        
+        
+    while True:
+        if not nic.isconnected():
+            print("No se ha conectado a la red")
+            rgb.color = convert(0,0,0)
+            await asyncio.sleep_ms(100)
+            continue
+        if pButton1.value() == 0 and pButton2.value() == 0:
+            print("Encontrar dispositivo")
+            rgb.color = convert(255,255,0)
+            code = "a".encode("utf-8")
+            s.send(create_request("a",url,"api","POST"))
+            print("Value a sent")
+        elif pButton1.value() == 0:
+            rgb.color = convert(0,255,255)
+            print("Haciendo grabación")
+            code = "b".encode("utf-8")
+            s.send(create_request("b",url,"api","POST"))
+            print("Notification sent")
+        elif pButton2.value() == 0:
+            print("Guardando grabacion")
+            rgb.color = convert(255,0,255)
+            code = "c".encode("utf-8")
+            s.send(create_request("c",url,"api","POST"))
+            print("Value sent")
+        else:
+            print("No se ha enviado nada de información")
+            rgb.color = convert(255,255,255)
+            code = "d".encode("utf-8")
+            #s.send(create_request("d",url,"api/command","GET"))
+            print(f"Value {code}")
+            
+        await asyncio.sleep_ms(500)
+            
 async def main():
+    
+    await connect_to_wifi() 
     tasks = [
         asyncio.create_task(remote_task()),
         asyncio.create_task(periphereal_task()),
-        asyncio.create_task(connection_watchdog()) 
+        asyncio.create_task(connection_watchdog())
     ]
     
+    
+    await send_to_API()
+        
+    
+
     await asyncio.gather(*tasks)
+    
         
 asyncio.run(main())
-            
-
-
-
-    
